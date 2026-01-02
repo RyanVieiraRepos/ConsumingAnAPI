d214513f1d02aec24878eb143fbf8964f2903eb9
ryanviera05 <ryanvieiramail@gmail.com>
2026-01-02 03:25:48 -0300

    Initial setup + planning

---
+++ Backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Olá Node')
+})
+
+
+//mandar pro banco um JSON
+app.post('/registrar', (req, res) => {
+
+})
+
+
+//pegar do banco e devolver lista de objetos
+app.get('/exibir', (req, res) => {
+
+    usuarios = {
+        nome: "Ryan",
+        sobrenome: "Vieira Assumpção",
+        idade: "20",
+        descrição: "brasileiro, nascido no interior de SP"
+    }
+
+    res.send(usuarios)
+
+
+})
+
+
+app.listen(3000)
---
+++ GeneralNotes/TechStack.md
@@ -0,0 +1,20 @@
+### FrontEnd - Vercel ( different repository)
+- HTML
+- Javascript
+- Css
+
+### Backend - Google Cloud
+- Node
+- Node Express
+- Node MySQL
+  %REST
+
+
+### Database - InfinityFree
+- MySql
+- DBeaver for Editing/Visualization
+
+
+
+
+# Cadastro e Exibição de usuários - Formulário -> Cards
